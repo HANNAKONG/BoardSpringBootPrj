f257727ac15a0c328294fadc296a4236113d517b
khn1346 <khn1346@gmail.com>
2024-08-08 19:58:52 +0900

    feat -> post 기본 기능 완료

--- src/main/java/com/hanna/first/springbootprj/web/BoardApiController.java
+++ src/main/java/com/hanna/first/springbootprj/web/BoardApiController.java
@@ -1,8 +1,11 @@
 package com.hanna.first.springbootprj.web;
 
+import com.hanna.first.springbootprj.domain.board.BoardType;
+import com.hanna.first.springbootprj.domain.post.PostStatus;
 import com.hanna.first.springbootprj.service.BoardService;
 import com.hanna.first.springbootprj.web.dto.BoardRequestDto;
 import com.hanna.first.springbootprj.web.dto.BoardResponseDto;
+import com.hanna.first.springbootprj.web.dto.PostRequestDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +19,13 @@ public class BoardApiController {
         this.boardService = boardService;
     }
 
+    // 회원권한-ADMIN인 경우 게시판 등록, 삭제, 이름변경 등 가능하게 추가?
     /**********************************
-     *  1. 게시글 목록 조회
+     *  게시판 등록
      **********************************/
-    @GetMapping("/api/v1/board")
-    public List<BoardResponseDto> getBoardList(@RequestBody BoardRequestDto requestDto){
-        return boardService.getBoardList(requestDto);
+    @PostMapping("/api/v1/board")
+    public void saveBoard(@RequestBody BoardRequestDto requestDto){
+        boardService.saveBoard(requestDto);
     }
 
 }
--- src/main/java/com/hanna/first/springbootprj/web/PostApiController.java
+++ src/main/java/com/hanna/first/springbootprj/web/PostApiController.java
@@ -1,9 +1,15 @@
 package com.hanna.first.springbootprj.web;
 
+import com.hanna.first.springbootprj.domain.board.BoardType;
+import com.hanna.first.springbootprj.domain.post.Post;
 import com.hanna.first.springbootprj.service.PostService;
+import com.hanna.first.springbootprj.web.dto.BoardRequestDto;
+import com.hanna.first.springbootprj.web.dto.BoardResponseDto;
 import com.hanna.first.springbootprj.web.dto.PostRequestDto;
 import com.hanna.first.springbootprj.web.dto.PostResponseDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,7 +22,8 @@ public class PostApiController {
     }
 
     /**********************************
-     *  1. 게시글 조회
+     *  테스트 필요
+     *  게시글 조회
      **********************************/
     @GetMapping("/api/v1/post/{id}")
     public PostResponseDto getPost(@PathVariable Long id){
@@ -24,7 +31,25 @@ public class PostApiController {
     }
 
     /**********************************
-     *  2. 게시글 등록
+     *  1. 게시글 목록 조회 - 게시판 유형별 조회
+     **********************************/
+    @GetMapping("/api/v1/boardList/{boardTypeCode}")
+    public List<PostResponseDto> getBoardList(@PathVariable BoardType boardTypeCode){
+        return postService.getBoardList(boardTypeCode);
+    }
+
+    /**********************************
+     *  테스트 필요
+     *  2. 게시글 목록 조회 - 아이디로 조회(임시저장/공개글)
+     **********************************/
+    @GetMapping("/api/v1/postList")
+    public List<Post> getPostList(@RequestBody PostRequestDto requestDto) {
+        System.out.println("controller========> "+requestDto.getUserId()+" "+requestDto.getPostStatusCode());
+        return postService.getPostList(requestDto);
+    }
+
+    /**********************************
+     *  3. 게시글 등록
      **********************************/
     @PostMapping("/api/v1/post")
     public void savePost(@RequestBody PostRequestDto requestDto){
@@ -32,20 +57,21 @@ public class PostApiController {
     }
 
     /**********************************
-     *  3. 게시글 수정
+     *  4. 게시글 수정
      **********************************/
     @PutMapping("/api/v1/post/{id}")
-    public void updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto){
-        postService.updatePost(id, requestDto);
+    public void updatePost(@RequestBody PostRequestDto requestDto){
+        postService.updatePost(requestDto);
     }
 
     /**********************************
-     *  4. 게시글 삭제
+     *  5. 게시글 삭제
      **********************************/
     @DeleteMapping("/api/v1/post/{id}")
     public void deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto){
         postService.deletePost(id, requestDto);
     }
+
 
 
 }
