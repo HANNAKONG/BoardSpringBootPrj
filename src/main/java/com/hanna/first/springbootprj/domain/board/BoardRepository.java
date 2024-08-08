42a0acfa648c56aa8e6d6ba770d1e3eb68c91301
khn1346 <khn1346@gmail.com>
2024-08-08 19:56:15 +0900

    feat -> 게시판 게시글 목록, 사용자 ID 및 게시글 상태별 조회 기능 완료

--- src/main/java/com/hanna/first/springbootprj/domain/board/BoardRepository.java
+++ src/main/java/com/hanna/first/springbootprj/domain/board/BoardRepository.java
@@ -1,12 +1,14 @@
 package com.hanna.first.springbootprj.domain.board;
 
-import com.hanna.first.springbootprj.web.dto.BoardRequestDto;
+import com.hanna.first.springbootprj.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findAllByBoardTypeCodeAndPostStatusCode(BoardRequestDto requestDto);
+
 }
--- src/main/java/com/hanna/first/springbootprj/domain/post/PostRepository.java
+++ src/main/java/com/hanna/first/springbootprj/domain/post/PostRepository.java
@@ -1,7 +1,18 @@
 package com.hanna.first.springbootprj.domain.post;
 
+import com.hanna.first.springbootprj.domain.board.BoardType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserId(String userId);
 
+    @Query("SELECT p FROM Post p JOIN p.board b WHERE b.boardTypeCode = :boardTypeCode")
+    List<Post> findAllWithPostsByBoardType(@Param("boardTypeCode") BoardType boardTypeCode);
+
+    @Query("SELECT p FROM Post p WHERE p.user.userId = :userId AND p.postStatusCode = :postStatusCode")
+    List<Post> findByUserIdAndPostStatus(@Param("userId") String userId, @Param("postStatusCode") PostStatus postStatusCode);
 }
