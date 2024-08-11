a6b5ac262abe2c92a05ff1f8e00cd1f043ff9bfd
khn1346 <khn1346@gmail.com>
2024-08-11 17:10:35 +0900

    feat → 게시판 기본 API 완성

--- src/main/java/com/hanna/first/springbootprj/web/BoardApiController.java
+++ src/main/java/com/hanna/first/springbootprj/web/BoardApiController.java
@@ -1,14 +1,8 @@
 package com.hanna.first.springbootprj.web;
 
-import com.hanna.first.springbootprj.domain.board.BoardType;
-import com.hanna.first.springbootprj.domain.post.PostStatus;
 import com.hanna.first.springbootprj.service.BoardService;
 import com.hanna.first.springbootprj.web.dto.BoardRequestDto;
-import com.hanna.first.springbootprj.web.dto.BoardResponseDto;
-import com.hanna.first.springbootprj.web.dto.PostRequestDto;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class BoardApiController {
@@ -19,13 +13,29 @@ public class BoardApiController {
         this.boardService = boardService;
     }
 
-    // 회원권한-ADMIN인 경우 게시판 등록, 삭제, 이름변경 등 가능하게 추가?
+    // 유저 권한: ADMIN인 경우 게시판 등록, 정보 변경, 삭제 가능
     /**********************************
-     *  게시판 등록
+     *  1. 게시판 등록
      **********************************/
     @PostMapping("/api/v1/board")
     public void saveBoard(@RequestBody BoardRequestDto requestDto){
         boardService.saveBoard(requestDto);
+    }
+
+    /**********************************
+     *  2. 게시판 정보 수정
+     **********************************/
+    @PutMapping("/api/v1/boardinfo/{id}")
+    public void updateBoardInfo(@PathVariable Long id, @RequestBody BoardRequestDto requestDto){
+        boardService.updateBoardInfo(id, requestDto);
+    }
+
+    /**********************************
+     *  3. 게시글 삭제
+     **********************************/
+    @DeleteMapping("/api/v1/board/{id}")
+    public void deleteBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto){
+        boardService.deleteBoard(id, requestDto);
     }
 
 }
