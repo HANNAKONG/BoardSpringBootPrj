fad5555ec197f69f7f1e6b166b5aa708a2dcbcc5
khn1346 <khn1346@gmail.com>
2024-08-15 00:44:39 +0900

    fix -> postList 필드 삭제

--- src/main/java/com/hanna/first/springbootprj/web/dto/BoardResponseDto.java
+++ src/main/java/com/hanna/first/springbootprj/web/dto/BoardResponseDto.java
@@ -11,7 +11,6 @@ public class BoardResponseDto {
     private Long id;
     private BoardType boardTypeCode;
     private String boardName;
-    private List<Post> postList;
 
     /**********************************
      *  constructor (entity to dto)
@@ -20,7 +19,6 @@ public class BoardResponseDto {
         this.id = entity.getId();
         this.boardTypeCode = entity.getBoardTypeCode();
         this.boardName = entity.getBoardName();
-        this.postList = entity.getPostList();
     }
 
     /**********************************
@@ -30,31 +28,11 @@ public class BoardResponseDto {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public BoardType getBoardTypeCode() {
         return boardTypeCode;
     }
 
-    public void setBoardTypeCode(BoardType boardTypeCode) {
-        this.boardTypeCode = boardTypeCode;
-    }
-
     public String getBoardName() {
         return boardName;
-    }
-
-    public void setBoardName(String boardName) {
-        this.boardName = boardName;
-    }
-
-    public List<Post> getPostList() {
-        return postList;
-    }
-
-    public void setPostList(List<Post> postList) {
-        this.postList = postList;
     }
 }
